82722569565420f34b2e62a917cc9671847f9642
eastevilhux <eastevilhux@163.com>
2021-01-22 16:47:04 +0800

    commit by hux

--- lib-uikit/src/main/java/com/god/uikit/commons/EastLessOnScrollListener.kt
+++ lib-uikit/src/main/java/com/god/uikit/commons/EastLessOnScrollListener.kt
@@ -1,120 +1,62 @@
 package com.god.uikit.commons
 
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.lang.NullPointerException
 
+class EastLessOnScrollListener : RecyclerView.OnScrollListener() {
 
-class EastLessOnScrollListener : RecyclerView.OnScrollListener {
+    companion object{
+        private const val TAG ="EastLessOnScrollListener==>"
+    }
 
-    //声明一个LinearLayoutManager
-    private var mLinearLayoutManager: LinearLayoutManager? = null
+    private var onScrollListener : OnLoadMoreListener? = null;
 
     //当前页，从0开始
     private var currentPage = 0;
-    //已经加载出来的Item的数量
-    private var totalItemCount = 0
-
-    //主要用来存储上一个totalItemCount
-    private var previousTotal = 0
-
-    //在屏幕上可见的item数量
-    private var visibleItemCount = 0
-
-    //在屏幕可见的Item中的第一个
-    private var firstVisibleItem = 0
-
-    //是否正在上拉数据
-    private var loading = true
-
-    private var onLoadMoreListener : OnLoadMoreListener? = null;
 
     private var onLoadMore : ((page:Int)->Unit)? = null;
+    private var onRefresh : (()->Unit)? = null;
 
-    constructor(mLinearLayoutManager : LinearLayoutManager){
-        this.mLinearLayoutManager = mLinearLayoutManager;
-    }
+    var isUpScroll = false //是否正在上滑标记
 
-    constructor(recyclerView: RecyclerView){
-        val layoutManager = recyclerView.layoutManager
-            ?: throw NullPointerException("the layoutManager of RecyclerView is null");
-        if(layoutManager is LinearLayoutManager){
-            this.mLinearLayoutManager = recyclerView.layoutManager as LinearLayoutManager;
-        }else{
-            throw IllegalAccessException("the layoutManager is not LinearLayoutManager");
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
+        val manager = recyclerView.getLayoutManager();
+        manager?.let {
+            val linearLayoutManager = it as LinearLayoutManager;
+            if (newState === RecyclerView.SCROLL_STATE_IDLE) { //当前未滑动
+                val itemCount: Int = manager.getItemCount() //总数
+                val lastItemPosition: Int = linearLayoutManager.findLastCompletelyVisibleItemPosition() //最后显示的位置
+                if (lastItemPosition == itemCount - 1 && isUpScroll) {
+                    onScrollListener?.onRefresh();
+                    onRefresh?.let {
+                        it.invoke();
+                    }
+                }
+                val fristItemPosition: Int = linearLayoutManager.findFirstCompletelyVisibleItemPosition() //第一个显示的位置
+                if (fristItemPosition == 0 && !isUpScroll) {
+                    currentPage++;
+                    onScrollListener?.onLoadMore(currentPage);
+                    onLoadMore?.let {
+                        it.invoke(currentPage);
+                    }
+                }
+            }
         }
-    }
-
-    constructor(mLinearLayoutManager : LinearLayoutManager,onLoadMoreListener : OnLoadMoreListener){
-        this.mLinearLayoutManager = mLinearLayoutManager;
-        this.onLoadMoreListener = onLoadMoreListener;
-    }
-
-    constructor(recyclerView: RecyclerView,onLoadMoreListener : OnLoadMoreListener){
-        val layoutManager = recyclerView.layoutManager
-            ?: throw NullPointerException("the layoutManager of RecyclerView is null");
-        if(layoutManager is LinearLayoutManager){
-            this.mLinearLayoutManager = recyclerView.layoutManager as LinearLayoutManager;
-        }else{
-            throw IllegalAccessException("the layoutManager is not LinearLayoutManager");
-        }
-        this.onLoadMoreListener = onLoadMoreListener;
     }
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-
-        visibleItemCount = recyclerView.getChildCount();
-        totalItemCount = mLinearLayoutManager?.getItemCount()?:0;
-        firstVisibleItem = mLinearLayoutManager?.findFirstVisibleItemPosition()?:0;
-        if(loading){
-            Log.d("wnwn","firstVisibleItem: " +firstVisibleItem);
-            Log.d("wnwn","totalPageCount:" +totalItemCount);
-            Log.d("wnwn", "visibleItemCount:" + visibleItemCount);
-
-            if(totalItemCount > previousTotal){
-                //说明数据已经加载结束
-                loading = false;
-                previousTotal = totalItemCount;
-            }
-        }
-        if (!loading && totalItemCount-visibleItemCount <= firstVisibleItem){
-            currentPage ++;
-            onLoadMoreListener?.let {
-                it.onLoadMore(currentPage);
-            }
-            onLoadMore?.let {
-                it.invoke(currentPage);
-            }
-            loading = true;
-        }
-    }
-
-    /**
-     * RecyclerView下拉加载监听事件
-     * create by Administrator at 2021/1/18 11:10
-     * @author Administrator
-     * @param onLoadMore
-     *      监听事件触发后的流程代码块
-     * @return
-     *      void
-     */
-    fun onLoadMore(onLoadMore:((page:Int)->Unit)){
-        this.onLoadMore = onLoadMore;
+        // 大于0表示正在向上滑动，小于等于0表示停止或向下滑动
+        isUpScroll = dy > 0;
     }
 
 
     interface OnLoadMoreListener{
-        /**
-         * RecyclerView下拉加载监听事件
-         * create by Administrator at 2021/1/18 11:08
-         * @author Administrator
-         * @param page
-         *      当前页下标
-         * @return
-         *      void
-         */
-        fun onLoadMore(page:Int);
+
+        fun onRefresh();
+
+        fun onLoadMore(currentPage: Int);
     }
+
 }
